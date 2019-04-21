ec540a5a1d42553dbab886f742d41116aa27a87a
melquick <melquick@gmail.com>
2019-04-21 17:43:16 -0400

    Combined GUIs

---
+++ CelloMini/app/src/main/java/com/example/cellomini/EnterWord.java
@@ -0,0 +1,64 @@
+package com.example.cellomini;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EnterWord extends Activity implements View.OnClickListener {
+
+    private Button ebutton1;
+
+    private EditText et;
+
+    String inputString;
+
+    @Override
+	/*An onCreate method/function is what is called when a screen is first showed.
+	  In this case, this is the main menu screen.
+	*/
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        //The super keyword is used to refer to the parent class in java
+        super.onCreate(savedInstanceState);
+
+        //How the activity actually looks is inside main.xml, inside the layout folder
+        setContentView(R.layout.enter_word);
+
+        //The buttons have parameters corresponding to the IDs in Main.xml
+        ebutton1 = (Button) findViewById(R.id.button_gen_circuit);
+
+        /*The buttons now have onClickListeners set, a method/function of the button class
+         * to start a new activity/intent when pressed. In this case, pressing a button
+         * will go to the results page.
+         * */
+        ebutton1.setOnClickListener(this);
+
+        // The edit text has a string value set by the editText1 ID in Main.xml
+        et = (EditText) findViewById(R.id.editText1);
+        inputString = et.getText().toString();
+    }
+
+    @Override
+    /*onClick is what is called when the buttons are pressed and they take in Views as arguments
+     * as buttons are children of the view class, buttons can polymorphically be passed in. The button
+     * that called the onClick is automatically fed in*/
+    public void onClick(View v)
+    {
+        //The switch statements grab the id values of the button pressed and calculates the tip accordingly
+        switch(v.getId()) {
+
+            case R.id.button_gen_circuit: {
+                //go to information page
+                Intent fillCircuitActivity = new Intent(EnterWord.this, FillCircuit.class);
+                fillCircuitActivity.putExtra("word", inputString);
+                startActivity(fillCircuitActivity);
+            }
+            default: {
+                break;
+            }
+        }
+    }
+}
---
+++ CelloMini/app/src/main/java/com/example/cellomini/MainActivity.java
@@ -0,0 +1,66 @@
+
+/*****************************************************************************************
+ * Adapted and tortured by Julian Trinh and Connor Richmond for use in ENG EC 327
+ * Source code from https://github.com/academicode/app-simple-tip-calculator/tree/session-7
+ * Sweet tutorial at https://www.youtube.com/watch?v=Z3jzIYkxB1s (where the source is from)
+ * Boston University: College of Engineering
+ *****************************************************************************************/
+package com.example.cellomini;
+
+//This is the class that will first be run when the app is first opened
+
+//import is #include from C++
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//Extends = inherits, implements means it interfaces with another class
+public class MainActivity extends Activity {
+
+    //Creating button objects, which extend (inherit) the View class
+    private Button mbutton1;
+    private Button mbutton2;
+
+    @Override
+	/*An onCreate method/function is what is called when a screen is first showed.
+	  In this case, this is the main menu screen.
+	*/
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        //The super keyword is used to refer to the parent class in java
+        super.onCreate(savedInstanceState);
+
+        //How the activity actually looks is inside main.xml, inside the layout folder
+        setContentView(R.layout.activity_main);
+
+        //The buttons have parameters corresponding to the IDs in Main.xml
+        mbutton1 = (Button) findViewById(R.id.button_get_started);
+        mbutton2 = (Button) findViewById(R.id.button_about_us);
+
+        /*The buttons now have onClickListeners set, a method/function of the button class
+         * to start a new activity/intent when pressed. In this case, pressing a button
+         * will go to the results page.
+         * */
+        mbutton1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tutorialActivity= new Intent(MainActivity.this, Tutorial.class);
+                startActivity(tutorialActivity);
+            }
+        });
+        mbutton2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent aboutUsActivity = new Intent(MainActivity.this, AboutUs.class);
+                startActivity(aboutUsActivity);
+            }
+        });
+    }
+}
