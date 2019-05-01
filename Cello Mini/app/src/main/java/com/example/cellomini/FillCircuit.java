1e74b474e7ad30cf3d6a3393dadd954269429ee2
celousan <celousan@bu.edu>
2019-04-30 22:22:31 -0400

    Revert "Merge pull request #11 from celousan/debug"
    This reverts commit 2f251536172bcdd5e6eb642418d01a9646977901, reversing
    changes made to a43160d12069d4f16f6eb49abccd0bec67c92318.

--- Cello Mini/app/src/main/java/com/example/cellomini/FillCircuit.java
+++ Cello Mini/app/src/main/java/com/example/cellomini/FillCircuit.java
@@ -92,6 +92,8 @@ public class FillCircuit extends Activity {
         evaluateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //debugging option for solution
+                //Log.d("LOGIC", Boolean.toString(wordInput.checkSol(toPromoterArray())));
 
                 if(wordInput.checkSol(toPromoterArray())){
                     //alert message with congratulatory message, exit button and return button.
@@ -131,9 +133,8 @@ public class FillCircuit extends Activity {
                     exitAlert.setNegativeButton("Display Answer", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            //get answer to circuit !!!!!
-                            Promoter[] solution = wordInput.getSol();
-                            //display answer to circuit
+                            //display answer to circuit !!!!!
+                            //wordInput.getSol();
                         }
                     });
                     exitAlert.create().show();
@@ -147,16 +148,16 @@ public class FillCircuit extends Activity {
 
         String temporaryText;
         int[] inputViewIds = new int[]{R.id.input1, R.id.input2, R.id.input3, R.id.input4, R.id.input5, R.id.input6, R.id.input7, R.id.input8};
-        Promoter[] promo = wordInput.getPromoters();
-        for(int i = 0; i < promo.length; i++)
+        Promoter[] p = wordInput.getPromoters();
+        for(int i = 0; i < p.length; i++)
         {
             TextView currentInput = findViewById(inputViewIds[i]);
-            if(promo[i].getLogic()) {
-                temporaryText = promo[i].getLet1() + " or " + promo[i].getLet2();
+            if(p[i].getLogic()) {
+                temporaryText = p[i].getLet1() + " or " + p[i].getLet2();
                 currentInput.setText(temporaryText);
             }
             else {
-                currentInput.setText(promo[i].getLet1());
+                currentInput.setText(p[i].getLet1());
             }
         }
 
@@ -179,11 +180,11 @@ public class FillCircuit extends Activity {
         for(int i = 0; i < 4 ; i++){
             TextView currentTarget = findViewById(targetViewIds[i]);
             //pass the string answer from the input textview back as a promoter
-            if(currentTarget.getText().length() > 1){
-                ansInputs[i] = new Promoter(currentTarget.getText().charAt(0),currentTarget.getText().charAt(5));
+            if(currentTarget.toString().length() > 1){
+                ansInputs[i] = new Promoter(currentTarget.toString().charAt(0),currentTarget.toString().charAt(5));
             }
             else{
-                ansInputs[i] = new Promoter(currentTarget.getText().charAt(0));
+                ansInputs[i] = new Promoter(currentTarget.toString().charAt(0));
             }
 
         }
